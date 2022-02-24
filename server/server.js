5d5a6eb9886c6f30674926d0153ea8dbf5ceff6a
tballmer <tad.ballmer@gmail.com>
2022-02-24 00:46:09 -0800

    Add project_member apis

--- server/server.js
+++ server/server.js
@@ -214,6 +214,91 @@ app.delete("/api/v1/projects/:id", async (req, res) => {
   }
 });
 
+// Get all project members
+app.get("/api/v1/project_members", async (req, res) => {
+  try {
+    const results = await db.query("select * from project_members");
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        project_members: results.rows,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Get a Project Member
+app.get("/api/v1/project_members/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "select * from project_members where id = $1",
+      [req.params.id]
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        project_member: results.rows,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Create a Project Member
+app.post("/api/v1/project_members", async (req, res) => {
+  try {
+    const results = await db.query(
+      "insert into project_members (project_id, member_id) values ($1, $2) returning *",
+      [req.body.project_id, req.body.member_id]
+    );
+    res.status(201).json({
+      status: "success",
+      data: {
+        project_member: results.rows[0],
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Update a Project Member
+app.put("/api/v1/project_members/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "Update project_members set project_id = $1, member_id = $2 where id = $3 returning *",
+      [req.body.project_id, req.body.member_id, req.params.id]
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        project_member: results.rows[0],
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// Delete a Project
+app.delete("/api/v1/project_members/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "delete from project_members where id = $1",
+      [req.params.id]
+    );
+    res.status(202).json({
+      status: "sucess",
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server has started on port ${port}`);
